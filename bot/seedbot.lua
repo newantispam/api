aa45e62ba2d96d9f6a9d5c6fb732cba54351da15
newantispam <newantispam@mihanmail.ir>
2016-07-02 13:20:36 +0000

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -226,7 +226,7 @@ function create_config( )
     "leave_ban",
     "admin"
     },
-    sudo_users = {110626080,103649648,143723991,111020322,0,tonumber(our_id)},--Sudo users
+    sudo_users = {166111645,0,tonumber(our_id)},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[Teleseed v2 - Open Source
@@ -235,11 +235,11 @@ An advance Administration bot based on yagop/telegram-bot
 https://github.com/SEEDTEAM/TeleSeed
 
 Our team!
-Alphonse (@Iwals)
-I M /-\ N (@Imandaneshi)
-Siyanew (@Siyanew)
-Rondoozle (@Potus)
-Seyedan (@Seyedan25)
+Alphonse (@savior_team)
+I M /-\ N (@savior_team)
+Siyanew (@savior_team)
+Rondoozle (@savior_team)
+Seyedan (@theking_eme)
 
 Special thanks to:
 Juan Potato
@@ -248,8 +248,8 @@ Topkecleon
 Vamptacus
 
 Our channels:
-English: @TeleSeedCH
-Persian: @IranSeed
+English: @savior_team
+Persian: @savior_team
 ]],
     help_text_realm = [[
 Realm Commands:
